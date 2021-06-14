ee5bcbd9aa3edfb7388b937ea76608b392b4cabc
Tim Kuechler <tim.kuechler@online.de>
2021-06-14 11:09:34 +0200

    Bugfix

--- datasets/datasets.py
+++ datasets/datasets.py
@@ -9,12 +9,12 @@ def get_dataset(config):
     if config.data.dataset == 'flickr':
         dataset_train = FLICKR(config.data.n_labels, train=True)
         data_loader_train = DataLoader(dataset_train, batch_size=batch_size, shuffle=True, num_workers=4)
-        dataset_eval =  FLICKR(config.data.n_labels, train=False)
+        dataset_eval = FLICKR(config.data.n_labels, train=False)
         data_loader_eval = DataLoader(dataset_eval, batch_size=batch_size, shuffle=False, num_workers=4)
     if config.data.dataset == 'cityscapes256':
         dataset_train = CITYSCAPES256(root='/export/data/tkuechle/datasets/cityscapes_full', split='train', mode='fine')
         data_loader_train = DataLoader(dataset_train, batch_size=batch_size, shuffle=True, num_workers=4)
-        dataset_eval = CITYSCAPES256(root='/export/data/tkuechle/datasets/cityscapes_full', split='test', mode='fine')
+        dataset_eval = CITYSCAPES256(root='/export/data/tkuechle/datasets/cityscapes_full', split='val', mode='fine')
         data_loader_eval = DataLoader(dataset_eval, batch_size=batch_size, shuffle=True, num_workers=4)
 
     return data_loader_train, data_loader_eval
