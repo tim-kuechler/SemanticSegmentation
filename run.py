44b49544d5b752cb601e7f653262c30014c36c93
Tim Kuechler <tim.kuechler@online.de>
2021-06-17 08:09:48 +0200

    D

--- run.py
+++ run.py
@@ -80,7 +80,7 @@ def train(config, workdir):
                 perturbed_img = mean + std[:, None, None, None] * z
                 max = torch.tensor(perturbed_img.shape[0])
                 for N in range(perturbed_img.shape[0]):
-                    max[N] = torch.max(perturbed_img[N,:,:,:])
+                    #max[N] = torch.max(perturbed_img[N,:,:,:])
                     print(torch.max(perturbed_img[N,:,:,:]))
                 perturbed_img = perturbed_img - torch.max(z, dim=1)[:, None, None, None] * torch.ones_like(img)
 
