122e0263985a974d10c1e10a99afc581b419513a
Tim Kuechler <tim.kuechler@online.de>
2021-06-21 10:52:17 +0200

    D

--- losses.py
+++ losses.py
@@ -36,7 +36,7 @@ def get_nll_loss(config):
                                      1.0166, 0.9969, 0.9754, 1.0489,
                                      0.8786, 1.0023, 0.9539, 0.9843,
                                      1.1116, 0.9037, 1.0865, 1.0955,
-                                     1.0865, 1.1529, 1.0507], device=config.device)
+                                     1.0865, 1.1529, 1.0507])
         return F.nll_loss(pred, targets, weight=weights, ignore_index=0)
 
     def nll_loss_flickr(pred, targets):
