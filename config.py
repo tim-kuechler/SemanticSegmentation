1789c2c86e41c45ab698a04f6c5e05b856772d1e
Tim Kuechler <tim.kuechler@online.de>
2021-06-21 11:19:13 +0200

    D

--- config.py
+++ config.py
@@ -56,7 +56,7 @@ def get_config_cityscapes():
     # Training
     config.training = training = ml_collections.ConfigDict()
     training.epochs = 5000
-    training.batch_size = 8
+    training.batch_size = 12
     training.log_freq = 20
     training.eval_freq = 500
     training.save_pred_freq = 1
